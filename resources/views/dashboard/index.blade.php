99801fee56e25fc41387576e627afeed85b185ea
bagussjm <bagusibag85@gmail.com>
2022-06-18 23:34:25 +0700

    add dashboard module

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.app')
+
+@section('judul_halaman', 'Halaman Utama')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body text-center ">
+                    <h4 class=" card-title float-left col-sm-12">
+                        SISTEM INFORMASI PENDUKUNG KEPUTUSAN PENURUNAN UKT UIN SUSKA RIAU
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
